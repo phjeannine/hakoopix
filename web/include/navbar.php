3759b5870d8327f1feb216509985f25c28345481
Amélie <amliebarre@gmail.com>
2016-01-12 08:47:05 +0100

    init projet on github

---
+++ web/include/navbar.php
@@ -0,0 +1,9 @@
+<!-- Navigation -->
+<nav class="navbar navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="navbar-content">
+            <h1><a href="#" class="navbar-brand">Logo</a></h1>
+            <a href="login.php" class="admin" title="Administration">Administration</a>
+        </div>
+    </div><!-- /.container -->
+</nav>
