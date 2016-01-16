66ce088b2361e6e85dec0a023866b1fd08c2d0e6
Amélie <amliebarre@gmail.com>
2016-01-16 18:50:43 +0100

    Finalisation de la homepage

--- web/views/index.php
+++ web/views/index.php
@@ -41,7 +41,7 @@
             <div class="holder">
                 <div class="inner">
                     <h1 class="contest-title">Titre du concours</h1>
-                    <p class="contest-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ligula mi, cursus non euismod ut, scelerisque ut ligula. Duis quis consequat tellus. Etiam commodo congue sem in accumsan. </p>
+                    <p class="contest-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ligula mi, cursus non euismod ut, scelerisque ut ligula. Duis quis consequat tellus. Etiam commodo congue sem in accumsan.</p>
                     <div class="fb-connect-button">
                         <span>Envie de jouer le jeu ?</span>
                         <a href="#" onClick="logInWithFacebook()">Je participe</a>
@@ -52,73 +52,106 @@
     </div><!-- /.overlay -->
 </div><!-- /.banner -->
 
+<!-- Homepage Content -->
+<div id="homepage" class="container">
 
-    <div class="container">
-    <!-- Projects Row -->
-    <div class="row">
-        <div class="text-center col-md-12">
-            <h3 class="page-header">Lots à gagner</h3>
+    <!-- Prices -->
+    <div id="prices" class="row">
+        <div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-6 col-xs-offset-3">
+            <h3 class="title">Lots à gagner</h3>
+            <hr>
+        </div>
+        <div class="price-item col-lg-4 col-md-4 col-sm-4 col-xs-4">
+            <img class="img-responsive" src="../public/images/price.jpg" alt="">
+            <h4 style="color: #12ba74">Premier prix</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ligula mi, cursus non euismod ut, scelerisque ut ligula.</p>
+        </div><!-- /.price-item -->
+
+        <div class="price-item col-lg-4 col-md-4 col-sm-4 col-xs-4">
+            <img class="img-responsive" src="../public/images/price.jpg" alt="">
+            <h4 style="color: #12ba74">Premier prix</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ligula mi, cursus non euismod ut, scelerisque ut ligula.</p>
+        </div><!-- /.price-item -->
+
+        <div class="price-item col-lg-4 col-md-4 col-sm-4 col-xs-4">
+            <img class="img-responsive" src="../public/images/price.jpg" alt="">
+            <h4 style="color: #12ba74">Premier prix</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ligula mi, cursus non euismod ut, scelerisque ut ligula.</p>
+        </div><!-- /.price-item -->
+    </div><!-- /.row -->
+
+    <!-- Gallery -->
+    <div id="gallery" class="row">
+        <div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-6 col-xs-offset-3">
+            <h3 class="title">Contributions</h3>
+            <hr>
         </div>
         <div class="col-md-4 portfolio-item">
-            <a href="#">
-                <img class="img-responsive imgLot" src="../public/images/lot1.jpg" alt="">
-            </a>
-            <h3 class="text-center">
-                <a href="#">Lot 1</a>
-            </h3>
+            <a href="#"><img class="img-responsive" src="../public/images/photo_contest.jpg" alt=""></a>
+            <div class="item-contest">
+                <div class="like item-contest-icon col-md-9"><span>like</span> 12</div>
+                <div class="share item-contest-icon col-md-3"><span>share</span> Partager</div>
+            </div>
+            <h3 class="item-title" style="color: #12ba74">Titre de la photo</h3>
+            <p class="item-meta">par <span style="color: #12ba74">{author}</span>, le <span style="color: #12ba74">{date}</span></p>
+            <hr>
+            <p class="item-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
         </div>
         <div class="col-md-4 portfolio-item">
-            <a href="#">
-                <img class="img-responsive imgLot" src="../public/images/lot2.jpg" alt="">
-            </a>
-            <h3 class="text-center">
-                <a href="#">Lot 2</a>
-            </h3>
+            <a href="#"><img class="img-responsive" src="../public/images/photo_contest.jpg" alt=""></a>
+            <div class="item-contest">
+                <div class="like item-contest-icon col-md-9"><span>like</span> 12</div>
+                <div class="share item-contest-icon col-md-3"><span>share</span> Partager</div>
+            </div>
+            <h3 class="item-title" style="color: #12ba74">Titre de la photo</h3>
+            <p class="item-meta">par <span style="color: #12ba74">{author}</span>, le <span style="color: #12ba74">{date}</span></p>
+            <hr>
+            <p class="item-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
         </div>
         <div class="col-md-4 portfolio-item">
-            <a href="#">
-                <img class="img-responsive imgLot" src="../public/images/lot1.jpg" alt="">
-            </a>
-            <h3 class="text-center">
-                <a href="#">Lot 3</a>
-            </h3>
-        </div>
-    </div>
-    <br>
-    <!-- Description -->
-    <div class="text-center col-md-12">
-        <h3 class="page-header">Description</h3>
-    </div>
-    <p class="text-center">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.
-    </p><br>
-    <hr>
-    <!-- Projects Row -->
-    <div class="row">
-      <div class="text-center col-md-12">
-          <h3 class="page-header">Contribution</h3>
-      </div>
-        <div class="col-md-4 portfolio-item">
-            <a href="#">
-                <img class="img-responsive" src="http://placehold.it/700x400" alt="">
-            </a>
+            <a href="#"><img class="img-responsive" src="../public/images/photo_contest.jpg" alt=""></a>
+            <div class="item-contest">
+                <div class="like item-contest-icon col-md-9"><span>like</span> 12</div>
+                <div class="share item-contest-icon col-md-3"><span>share</span> Partager</div>
+            </div>
+            <h3 class="item-title" style="color: #12ba74">Titre de la photo</h3>
+            <p class="item-meta">par <span style="color: #12ba74">{author}</span>, le <span style="color: #12ba74">{date}</span></p>
+            <hr>
+            <p class="item-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
         </div>
         <div class="col-md-4 portfolio-item">
-            <a href="#">
-                <img class="img-responsive" src="http://placehold.it/700x400" alt="">
-            </a>
+            <a href="#"><img class="img-responsive" src="../public/images/photo_contest.jpg" alt=""></a>
+            <div class="item-contest">
+                <div class="like item-contest-icon col-md-9"><span>like</span> 12</div>
+                <div class="share item-contest-icon col-md-3"><span>share</span> Partager</div>
+            </div>
+            <h3 class="item-title" style="color: #12ba74">Titre de la photo</h3>
+            <p class="item-meta">par <span style="color: #12ba74">{author}</span>, le <span style="color: #12ba74">{date}</span></p>
+            <hr>
+            <p class="item-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
         </div>
         <div class="col-md-4 portfolio-item">
-            <a href="#">
-                <img class="img-responsive" src="http://placehold.it/700x400" alt="">
-            </a>
+            <a href="#"><img class="img-responsive" src="../public/images/photo_contest.jpg" alt=""></a>
+            <div class="item-contest">
+                <div class="like item-contest-icon col-md-9"><span>like</span> 12</div>
+                <div class="share item-contest-icon col-md-3"><span>share</span> Partager</div>
+            </div>
+            <h3 class="item-title" style="color: #12ba74">Titre de la photo</h3>
+            <p class="item-meta">par <span style="color: #12ba74">{author}</span>, le <span style="color: #12ba74">{date}</span></p>
+            <hr>
+            <p class="item-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
         </div>
-    </div>
-    <hr>
-    <!-- /.row -->
+        <div class="col-md-4 portfolio-item">
+            <a href="#"><img class="img-responsive" src="../public/images/photo_contest.jpg" alt=""></a>
+            <div class="item-contest">
+                <div class="like item-contest-icon col-md-9"><span>like</span> 12</div>
+                <div class="share item-contest-icon col-md-3"><span>share</span> Partager</div>
+            </div>
+            <h3 class="item-title" style="color: #12ba74">Titre de la photo</h3>
+            <p class="item-meta">par <span style="color: #12ba74">{author}</span>, le <span style="color: #12ba74">{date}</span></p>
+            <hr>
+            <p class="item-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+        </div>
+    </div><!-- /.row -->
 
 </div><!-- /.container -->
