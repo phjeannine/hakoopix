308a501c68380a7dc2b11f9d0fa9af351c9f1766
ChiTai <chitai95@hotmail.fr>
2016-02-01 08:37:00 +0100

    Revert "Modification fichier layout.php"
    This reverts commit 4f8dafc06cd3804427d04fc6139f3eb1086b6b89.

--- web/views/layout.php
+++ web/views/layout.php
@@ -18,38 +18,38 @@ $uri = $_SERVER['REQUEST_URI'];
     <title>Hakoopix | Concours Photo Facebook en ligne</title>
 
     <!-- CSS Stylesheets -->
-    <link href="<?php __ROOT__?>/public/css/jquery-ui.min.css" rel="stylesheet">
-    <link href="<?php __ROOT__?>/public/css/animate.min.css" rel="stylesheet">
+    <link href="../public/css/jquery-ui.min.css" rel="stylesheet">
+    <link href="../public/css/animate.min.css" rel="stylesheet">
     <!-- Bootstrap Core CSS -->
-    <link href="<?php __ROOT__?>/public/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../public/css/bootstrap.min.css" rel="stylesheet">
     <?php if($uri != "/dashboard" && $uri != "/addContest" && $uri != "/addAdmin" && $uri != "/contestList" && $uri != "/addPrice") { ?>
-      <link href="<?php __ROOT__?>/public/css/3-col-portfolio.css" rel="stylesheet">
+      <link href="../public/css/3-col-portfolio.css" rel="stylesheet">
     <?php } ?>
 
     <!-- General Stylesheets -->
     <?php if($uri != "/dashboard" && $uri != "/addContest" && $uri != "/addAdmin" && $uri != "/contestList" && $uri != "/addPrice") { ?>
-      <link href="<?php __ROOT__?>/public/css/main.css" rel="stylesheet">
+      <link href="../public/css/main.css" rel="stylesheet">
     <?php } ?>
-    <link href="<?php __ROOT__?>/public/css/home.css" rel="stylesheet">
-    <link href="<?php __ROOT__?>/public/css/contest.css" rel="stylesheet">
-    <link href="<?php __ROOT__?>/public/css/login.css" rel="stylesheet">
-    <link href="<?php __ROOT__?>/public/css/addAdmin.css" rel="stylesheet">
-    <link href="<?php __ROOT__?>/public/css/participate.css" rel="stylesheet">
-    <link href="<?php __ROOT__?>/public/css/dashboard.css" rel="stylesheet">
+    <link href="../public/css/home.css" rel="stylesheet">
+    <link href="../public/css/contest.css" rel="stylesheet">
+    <link href="../public/css/login.css" rel="stylesheet">
+    <link href="../public/css/addAdmin.css" rel="stylesheet">
+    <link href="../public/css/participate.css" rel="stylesheet">
+    <link href="../public/css/dashboard.css" rel="stylesheet">
 
     <!-- SB Admin -->
-    <link href="<?php __ROOT__?>/templates/sb-admin/css/sb-admin.css" rel="stylesheet">
-    <link href="<?php __ROOT__?>/templates/sb-admin/css/plugins/morris.css" rel="stylesheet">
-    <link href="<?php __ROOT__?>/templates/sb-admin/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="../templates/sb-admin/css/sb-admin.css" rel="stylesheet">
+    <link href="../templates/sb-admin/css/plugins/morris.css" rel="stylesheet">
+    <link href="../templates/sb-admin/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
     <!-- Fonts -->
-    <link href="<?php __ROOT__?>/public/fonts/opensans.css" rel="stylesheet">
-    <link href="<?php __ROOT__?>/public/fonts/montserrat.css" rel="stylesheet">
+    <link href="../public/fonts/opensans.css" rel="stylesheet">
+    <link href="../public/fonts/montserrat.css" rel="stylesheet">
 
     <!-- SCRIPTS -->
-    <script src="<?php __ROOT__?>/public/js/jquery-1.12.0.min.js" type="text/javascript"></script>
-    <script src="<?php __ROOT__?>/public/js/jquery-ui.min.js" type="text/javascript"></script>
-    <script src="<?php __ROOT__?>/public/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="../public/js/jquery-1.12.0.min.js" type="text/javascript"></script>
+    <script src="../public/js/jquery-ui.min.js" type="text/javascript"></script>
+    <script src="../public/js/bootstrap.min.js" type="text/javascript"></script>
 
 </head>
 <body cz-shortcut-listen="true">
@@ -75,16 +75,16 @@ $uri = $_SERVER['REQUEST_URI'];
                       <?php if($_SESSION["role"] == "admin") { ?>
                         <li><a href="/dashboard" title="Administration">Administration</a></li>
                         <hr>
-                        <li><a href="<?php __ROOT__?>/public/data/disconnect.php">Déconnexion</a></li>
+                        <li><a href="../public/data/disconnect.php">Déconnexion</a></li>
                       <?php } else { ?>
                         <li><a href="/participate" title="Administration">Participer</a></li>
                         <li><a href="/participate" title="Administration">Mes photos</a></li>
                         <hr>
-                        <li><a href="<?php __ROOT__?>/public/data/disconnect.php">Déconnexion</a></li>
+                        <li><a href="../public/data/disconnect.php">Déconnexion</a></li>
                       <?php } ?>
                     </ul>
                   </li>
-                  <li><a href="<?php __ROOT__?>/public/data/disconnect.php"><i class="fa fa-power-off"></i></a></li>
+                  <li><a href="../public/data/disconnect.php"><i class="fa fa-power-off"></i></a></li>
                 </ul>
                 <?php } ?>
               </div><!-- /.navbar-collapse -->
@@ -102,13 +102,13 @@ $uri = $_SERVER['REQUEST_URI'];
   </div><!-- /.site-wrapper -->
 
   <!-- SCRIPTS -->
-  <script src="<?php __ROOT__?>/public/js/script.js" type="text/javascript"></script>
-  <script src="<?php __ROOT__?>/public/js/functions.js" type="text/javascript"></script>
-  <script src="<?php __ROOT__?>/public/js/jscolor.js" type="text/javascript"></script>
-  <script src="<?php __ROOT__?>/templates/sb-admin/js/plugins/morris/raphael.min.js"></script>
+  <script src="../public/js/script.js" type="text/javascript"></script>
+  <script src="../public/js/functions.js" type="text/javascript"></script>
+  <script src="../public/js/jscolor.js" type="text/javascript"></script>
+  <script src="../templates/sb-admin/js/plugins/morris/raphael.min.js"></script>
   <script src="http://code.highcharts.com/highcharts.js"></script>
-  <!-- <script src="<?php __ROOT__?>/templates/sb-admin/js/plugins/morris/morris.min.js"></script> -->
-  <!-- <script src="<?php __ROOT__?>/templates/sb-admin/js/plugins/morris/morris-data.js"></script> -->
+  <!-- <script src="../templates/sb-admin/js/plugins/morris/morris.min.js"></script> -->
+  <!-- <script src="../templates/sb-admin/js/plugins/morris/morris-data.js"></script> -->
 
 </body>
 </html>
