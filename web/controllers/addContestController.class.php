0ec07862eb607270b3ed4617dfc9a74c1d7f0bfb
ChiTai <chitai95@hotmail.fr>
2016-02-11 02:13:28 +0100

    Résolution bug addContestController

--- web/controllers/addContestController.class.php
+++ web/controllers/addContestController.class.php
@@ -8,6 +8,7 @@ class addContestController {
 	}
 
 	public function insertAction($args){
+		$id = '0';
 		$title = $_POST['title'];
 		$date_begin = $_POST['begin'];
 		$date_ending = $_POST['ending'];
@@ -15,17 +16,17 @@ class addContestController {
 		$color_theme = $_POST['color-theme'];
 		$logo = $_FILES['logo']['name'];
 		$banner = $_FILES['banner']['name'];
-		$active_contest = "false";
-		$delete_contest = "false";
+		$active_contest = 0;
+		$delete_contest = 0;
 
 		if(!empty($_POST['active-contest'])) {
 			$active_contest = $_POST['active-contest'];
 		}
 
-		$addContestObj = new contestModel($title, $date_begin, $date_ending, $description, $color_theme, $logo, $banner, $active_contest, $delete_contest);
+		$addContestObj = new contestModel($id, $title, $date_begin, $date_ending, $description, $color_theme, $logo, $banner, $active_contest, $delete_contest);
 		$addContestObj->save();
 
-		//header("Location: /contestList");
+		header("Location: /contestList");
 	}
 
 }
