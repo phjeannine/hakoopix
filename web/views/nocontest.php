17be3638d48635a255f5d7d7846727bd02a6e7db
ChiTai <chitai95@hotmail.fr>
2016-02-23 01:46:19 +0100

    Connexion Admin

--- web/views/nocontest.php
+++ web/views/nocontest.php
@@ -1,3 +1,46 @@
+<script>
+	logInWithFacebook = function() {
+		FB.login(function(response) {
+			if (response.authResponse) {
+				console.log('You are logged!');
+				//console.log(response.authResponse);
+
+				FB.api('/me?fields=id,last_name,first_name,picture', function(rep) {
+					window.location="../public/createSessionAdmin.php?id="+rep.id+"&last_name="+rep.last_name+"&first_name="+rep.first_name+"&picture="+rep.picture+"&token="+response.authResponse.accessToken;
+
+					// A ne pas décommenter la requête ajax
+					/* $.ajax({
+					 url:'../public/createSession.php',
+					 data: {id:rep.id, last_name:rep.last_name, first_name:rep.first_name, picture:rep.picture, token:response.authResponse.accessToken},
+					 success: function(reponse) {
+					 window.location="/contest/adduser";
+					 }
+
+					 });
+					 */
+				});
+			}
+		},{scope: 'user_photos'});
+		return false;
+	};
+
+	window.fbAsyncInit = function() {
+		FB.init({
+			appId: "959119600818575",
+			cookie: true, // This is important
+			version: 'v2.5'
+		});
+	};
+
+	(function(d, s, id){
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) {return;}
+		js = d.createElement(s); js.id = id;
+		js.src = "//connect.facebook.net/fr_FR/sdk.js";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+</script>
+
 <div id="nocontest">
 	<div id="content">
 		<img src="../public/images/logo.png" title="logo Hakoopix">
